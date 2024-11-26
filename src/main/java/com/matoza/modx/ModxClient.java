128dd4bb42b67da90049ef84c2f8eba4bdc1f0e4
raf5000 <rbfmatoza@mymail.mapua.edu.ph>
2024-11-26 19:55:36 +0800

    initial commit

---
+++ src/main/java/com/matoza/modx/ModxClient.java
@@ -0,0 +1,11 @@
+package com.matoza.modx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Client Started");
+
+    }
+}
